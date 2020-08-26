b4b1c7b567f8a23d8e482d0c811b99c4235ae1ea
IshitwaAgrawal <ishitwa2000@gmail.com>
2020-08-26 12:23:13 +0530

    added animation and stars

--- script.js
+++ script.js
@@ -6,14 +6,8 @@ var pos = 100 + "px";
 
 r.style.transform = "translate(" + pos + ")";
 
-const moving = setInterval(() => {
-    r.style.transform = "translateY(" + -t + "px)";
-    t++;
-}, 100);
-
 
 function move() {
-    clearInterval(moving);
     moveUp();
     moveDown();
 }
@@ -28,6 +22,29 @@ function moveUp() {
     setTimeout(moveUp, 130);
 }
 
+function animateStars(k) {
+    let j = 1;
+    let v = 10;
+    setInterval(() => {
+        k.style.transform = "translateY(" + j + "px)";
+        j += v;
+    }, 100);
+}
 
-
+function stars() {
+    var n = 50;
+    for (let i = 1; i <= n; i++) {
+        let z = document.createElement('i');
+        z.style.position = "absolute";
+        z.style.top = Math.random() * window.innerHeight + "px";
+        z.style.left = Math.random() * window.innerWidth + "px";
+        z.style.height = 4 + "px";
+        z.style.width = 1 + "px";
+        z.style.backgroundColor = "white";
+        z.style.transform = "translateY(-10px)";
+        animateStars(z);
+        c.appendChild(z);
+    }
+}
+setInterval(stars, 2000);
 setTimeout(move, 1000);
