d9605da7c4dcc95f581dc1e8dbea913c5ce4ae87
IshitwaAgrawal <ishitwa2000@gmail.com>
2020-08-26 11:57:12 +0530

    added the moving effect

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
+var c = document.querySelector('.canvas');
 var r = document.querySelector(".rocket");
 var k = true;
+var t = 1;
+var pos = 100 + "px";
 
-function change() {}
+r.style.transform = "translate(" + pos + ")";
 
-setInterval(change, 100);
+const moving = setInterval(() => {
+    r.style.transform = "translateY(" + -t + "px)";
+    t++;
+}, 100);
+
+
+function move() {
+    clearInterval(moving);
+    moveUp();
+    moveDown();
+}
+
+function moveDown() {
+    r.style.transform = "translateY(1px)";
+    setTimeout(moveDown, 100);
+}
+
+function moveUp() {
+    r.style.transform = "translateY(-1px)";
+    setTimeout(moveUp, 130);
+}
+
+
+
+setTimeout(move, 1000);
